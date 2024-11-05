cdf436693ea0c9ec0c51bf2043d5d1f604d8984c
AbuBokorprog <abubokor1066@gmail.com>
2024-11-05 09:20:44 +0600

    Chapter-1

---
+++ chapter-1/module.py
@@ -0,0 +1,12 @@
+# module like js library
+import pyjokes
+
+# Singleline comment:
+# print("Printing jokes....")
+
+# Multiline comment
+"""This is multi 
+line comments."""
+
+joke = pyjokes.get_joke()
+print(joke)
